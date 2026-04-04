1f8a1f913e1921af2efeee4a56efa018bb58f3ca
LaleliloluGit <lolailococoliso@gmail.com>
2026-04-04 20:02:25 +0200

    Mejora de estilos

--- sensia/src/components/pages/home/Home.jsx
+++ sensia/src/components/pages/home/Home.jsx
@@ -2,15 +2,17 @@ import { Outlet } from "react-router-dom"
 import Header from "../../common/Header"
 
 function Home() {
-
   return (
-    <div className="h-screen w-screen relative">
-      {/* Div gigante centrado */}
+    <div className="min-h-screen w-screen relative">
       <div className="w-screen h-screen fixed top-0 left-0 overflow-hidden">
         <div className="arcoiris absolute w-[300vw] h-[300vh] -z-20"></div>
       </div>
+
       <Header />
-      <Outlet />
+
+      <main className="relative z-10 pt-28 md:pt-32">
+        <Outlet />
+      </main>
     </div>
   )
 }
